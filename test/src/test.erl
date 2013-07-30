03e5abbc0abb4f5bb111b033a5807f80a130bfc8
Matthew Sackman <matthew@rabbitmq.com>
2013-07-30 18:09:41 +0100

    Starting to get going on dsl interpreter for the tests.

--- test/src/test.erl
+++ test/src/test.erl
@@ -4,44 +4,57 @@
 
 -include("rabbit_clusterer.hrl").
 
--record(edge, {config, pre, post}).
+%% {par, [DoAllTheseInParallel]}
+%% {seq, [SequenceTheseThings]}
+
+interpret({seq, []}, State) ->
+    State;
+interpret({seq, [H|T]}, State) ->
+    interpret({seq, T}, interpret(H, State));
+interpret({par, []}, State) ->
+    State;
+interpret({par, P}, State) ->
+    Ref = make_ref(),
+    Self = self(),
+    Fun = fun (Result) -> Self ! {Ref, self(), Result}, ok end,
+    PidTasks =
+        orddict:from_list(
+          [{spawn(fun () -> Fun(interpret(T, State)) end), T} || T <- P]),
+    merge_results(gather(PidTasks, Ref), State);
+interpret({merge_results, Task, TaskState}, State) ->
+    %% todo
+    State;
+interpret(extend, State) ->
+    interpret(generate(State));
+interpret({exec, Fun}, State) ->
+    Fun(State).
+
+gather(PidTasks, Ref) ->
+    MRefs = [monitor(process, Pid) || Pid <- orddict:fetch_keys(PidTasks)],
+    Results = gather(PidTasks, Ref, orddict:new()),
+    [demonitor(MRef, [flush]) || MRef <- MRefs],
+    Results.
+
+gather(PidTasks, Ref, Results) ->
+    case orddict:size(PidTasks) of
+        0 -> Results;
+        _ -> receive
+                 {Ref, Pid, Result} ->
+                     gather(orddict:erase(Pid, PidTasks), Ref,
+                            orddict:store(orddict:fetch(Pid, PidTasks),
+                                          Result, Results));
+                 {'DOWN', _MRef, process, Pid, _Info} ->
+                     gather(orddict:erase(Pid, PidTasks), Ref,
+                            orddict:update(orddict:fetch(Pid, PidTasks),
+                                           fun id/1, failed, Results))
+             end
+    end.
+
+merge_results(TaskResults, State) ->
+    interpret(
+      {seq, [{merge, T, R} || {T, R} <- orddict:to_list(TaskResults)]}, State).
+
+id(X) -> X.
 
 test() ->
-    io:format("~p~n", [node()]),
     ok.
-
-transitions() ->
-    [add_node,
-     remove_node,
-     merge_node,
-     unmerge_node,
-     restart_node,
-     change_timeout,
-     change_gospel,
-     reset_cluster].
-
-new_edge() ->
-    #edge { config = #config { version          = 0,
-                               gospel           = reset,
-                               nodes            = [],
-                               shutdown_timeout = infinity },
-            pre    = [],
-            post   = [] }.
-
-add_node(Edge = #edge { config = Config = #config { nodes = Nodes },
-                        post = Post }) ->
-    %% this is a brand new virgin node coming up and joining into our
-    %% cluster.
-    Node = new_node(),
-    Edge #edge { config = Config #config { nodes = [Node | Nodes] },
-                 post   = [verify_cluster_includes(Node) | Post] }.
-
-merge_node(Edge = #edge { config = Config = #config { nodes = Nodes },
-                          pre = Pre, post = Post }) ->
-    %% We start a brand new node without the config, we then later
-    %% verify that it has been joined into our config
-    Node = new_node(),
-    Edge #edge { pre   = [start_node(Node, undefined) | Pre],
-                 post  = [verify_cluster_includes(Node) | Post],
-                 config = Config #config { nodes = [Node | Nodes] } }.
-
