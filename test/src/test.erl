f01f36d5938ed9d84a060b8cf6f228e6acb3efc0
Matthew Sackman <matthew@rabbitmq.com>
2013-07-31 18:13:31 +0100

    wip

--- test/src/test.erl
+++ test/src/test.erl
@@ -4,8 +4,16 @@
 
 -include("rabbit_clusterer.hrl").
 
-%% {par, [DoAllTheseInParallel]}
-%% {seq, [SequenceTheseThings]}
+-record(test, { seed,
+                program,
+                cluster_state
+              }).
+-record(cluster, { nodes_state,
+                   config
+                 }).
+
+-record(node_state, { config,
+                      running }).
 
 interpret({seq, []}, State) ->
     State;
@@ -24,8 +32,6 @@ interpret({par, P}, State) ->
 interpret({merge_results, Task, TaskState}, State) ->
     %% todo
     State;
-interpret(extend, State) ->
-    interpret(generate(State));
 interpret({exec, Fun}, State) ->
     Fun(State).
 
@@ -54,7 +60,133 @@ merge_results(TaskResults, State) ->
     interpret(
       {seq, [{merge, T, R} || {T, R} <- orddict:to_list(TaskResults)]}, State).
 
+generate(Seed) ->
+    Test = #test { seed          = Seed,
+                   program       = [],
+                   cluster_state =
+                       #cluster { nodes_state = [],
+                                  config =
+                                      #config { nodes            = [],
+                                                shutdown_timeout = infinity,
+                                                gospel           = reset,
+                                                version          = 0 }
+                                }
+                 },
+    Test1 = generate_program(Test),
+    Test1 #test { seed = Seed }.
+
+generate_program(Test = #test { program = Prog, seed = 0 }) ->
+    Test #test { program = lists:reverse(Prog) };
+generate_program(Test = #test { program       = Prog,
+                                cluster_state = CS,
+                                seed          = Seed }) ->
+    {{Instr, CS1}, Seed1} = choose_one(
+                              Seed,
+                              lists:append(
+                                [change_shutdown_timeout_instructions(CS),
+                                 change_gospel_instructions(CS),
+                                 add_node_to_cluster_instructions(CS),
+                                 remove_node_from_cluster_instructions(CS),
+                                 start_node_instructions(CS),
+                                 stop_node_instructions(CS),
+                                 delete_node_instructions(CS),
+                                 await_termination_instructions(CS),
+                                 reset_node_instructions(CS)])),
+    generate_program(Test #test { program       = [Instr | Prog],
+                                  seed          = Seed1,
+                                  cluster_state = CS1 }).
+
+choose_one(N, List) ->
+    Len = length(List),
+    {lists:nth(1 + (N rem Len), List), N div Len}.
+
+change_shutdown_timeout_instructions(CS = #cluster { config = Config = #config { shutdown_timeout = X }}) ->
+    InterestingValues = [infinity, 0, 1, 2, 10, 30],
+    [{{change_shutdown_timeout, V},
+      CS #cluster { config = Config #config { shutdown_timeout = V }}}
+     || V <- InterestingValues, V =/= X ].
+
+change_gospel_instructions(CS = #cluster { config = Config = #config { gospel = X, nodes = Nodes }}) ->
+    Values = [reset | [N || {N, _} <- Nodes]],
+    [{{change_gospel, V},
+      CS #cluster { config = Config #config { gospel = V }}}
+     || V <- Values, V =/= X ].
+
+add_node_to_cluster_instructions(CS = #cluster { nodes_state = NodesState,
+                                                config = Config = #config { nodes = ConfigNodes }}) ->
+    NewNode = generate_node(orddict:fetch_keys(NodesState)),
+    Nodes = [NewNode | [N || {N, _NodeState} <- NodesState, not orddict:is_key(N, ConfigNodes)]],
+    [{{add_node_to_cluster, N},
+     CS #cluster { config = Config #config { nodes = [{N, disc} | ConfigNodes] } }} || N <- Nodes].
+
+remove_node_from_cluster_instructions(CS = #cluster { config = Config = #config { nodes = ConfigNodes }}) ->
+    [{{remove_node_from_cluster, N},
+      CS #cluster { config = Config #config { nodes = orddict:erase(N, ConfigNodes) } }}
+     || {N, _} <- ConfigNodes].
+
+start_node_instructions(CS = #cluster { nodes_state = NodesState, config = Config }) ->
+    NewNode = generate_node(orddict:fetch_keys(NodesState)),
+    Nodes = [{NewNode, #node_state { running = false,
+                                     config  = default }}
+             | [{N, NS} || {N, NS = #node_state { running = false }} <- NodesState]],
+    Update = fun (N, NS = #node_state { config = ConfigOrig }, ApplyConfig) ->
+                     NewConfig = #config { nodes = ClusterNodes, shutdown_timeout = T } =
+                         case ApplyConfig of
+                             true  -> Config;
+                             false -> ConfigOrig
+                         end,
+                     NS #node_state { config = NewConfig,
+                                      running = case orddict:is_key(N, ClusterNodes) of
+                                                    true  -> true;
+                                                    false -> {terminating, T}
+                                                end }
+             end,
+    %% TODO: if ApplyConfig then that will spread via N. Need to track.
+    [{{start_node, N, ApplyConfig},
+      CS #cluster { nodes_state = orddict:store(N, Update(N, NS, ApplyConfig), NodesState) }
+     } || {N, NS} <- Nodes, ApplyConfig <- [true, false]].
+
+stop_node_instructions(CS = #cluster { nodes_state = NodesState }) ->
+    Stops = [{{stop_node, N}, N, NS #nodes_state { running = false }}
+             || {N, NS = #node_state { running = R }} <- NodesState, R =/= false ],
+    case Stops of
+        [] ->
+            [];
+        [{Instr, N, NS}] ->
+            [{Instr, CS #cluster { nodes_state = orddict:store(N, NS, NodesState) }}];
+        [{AI,A,ANS},{BI,B,BNS}|Ts] -> [A, {par, [A, B]}, {par, Stops}]
+    end.
+
+delete_node_instructions(CS = #cluster { nodes_state = NodesState }) ->
+    [{{delete_node, N},
+      CS #cluster { nodes_state = orddict:erase(N, NodesState) }
+     } || {N, #node_state { running = false }} <- NodesState ].
+
+await_termination_instructions(CS = #cluster { nodes_state = NodesState }) ->
+    [{{await_termination, N, T},
+      CS #cluster { nodes_state =
+                        orddict:store(N, NS #node_state { running = false }, NodesState) }
+     } || {N, NS = #node_state { running = {terminating, T} }} <- NodesState, T =/= infinity ].
+        
+reset_node_instructions(CS = #cluster { nodes_state = NodesState,
+                                        config = Config = #config { nodes = ConfigNodes } }) ->
+    FindConfig = fun (N) ->
+                         case orddict:is_key(N, ConfigNodes) of
+                             true  -> Config;
+                             false -> default
+                         end
+                 end,
+    [{{reset_node, N},
+      CS #cluster { nodes_state = orddict:store(
+                                    N, NS #node_state { config = FindConfig(N) }, NodesState) }
+     } || {N, NS = #node_state { config = C, running = false }} <- NodesState, C =/= default].
+
 id(X) -> X.
 
 test() ->
-    ok.
+    case node() of
+        'nonode@nohost' ->
+            {error, must_be_distributed_node};
+        _ ->
+            ok
+    end.
