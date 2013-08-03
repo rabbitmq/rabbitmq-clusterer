afb722386afdebd3b9b40a3c62eca80c15f8583c
Matthew Sackman <matthew@rabbitmq.com>
2013-08-04 00:45:40 +0100

    That's possibly the config evolver done.

--- test/src/config_evolve.erl
+++ test/src/config_evolve.erl
@@ -11,6 +11,8 @@
 
 -include("rabbit_clusterer.hrl").
 
+-define(NOOP_WEIGHT, 4).
+
 observe(Pid, Seed) ->
     gen_server:call(Pid, {observe, Seed}, infinity).
 
@@ -26,8 +28,22 @@ start_link(Node, Config = #config {}, Namer) ->
 init([Node, Config, Namer]) ->
     {ok, #state {config = Config, node = Node, node_namer = Namer}}.
 
-handle_call({observe, Seed}, _From, State) ->
-    {reply, Seed, State};
+handle_call({observe, Seed}, _From, State = #state { config     = Config,
+                                                     node_namer = Namer }) ->
+    {Config1, Seed1} = choose_one(
+                         Seed,
+                         lists:append(
+                           [change_shutdown_timeout(Config),
+                            change_gospel(Config),
+                            add_node_to_cluster(Config, Namer),
+                            remove_node_from_cluster(Config),
+                            lists:duplicate(?NOOP_WEIGHT, Config)])),
+    %% Some of the options are deliberately lazy, so eval here
+    Config2 = case is_function(Config1) of
+                  true  -> Config1();
+                  false -> Config1
+              end,
+    {reply, Seed1, State #state { config = Config2 }};
 handle_call(transmogrify, _From, State) ->
     %% This is really a noop as we apply the selected change in
     %% observe. Application of a config to a node is driven by the
@@ -49,3 +65,31 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
+choose_one(N, List) ->
+    Len = length(List),
+    {lists:nth(1 + (N rem Len), List), N div Len}.
+
+change_shutdown_timeout(Config = #config { shutdown_timeout = X }) ->
+    Values = [infinity, 0, 1, 2, 10, 30],
+    [Config #config { shutdown_timeout = V } || V <- Values, V =/= X ].
+    
+change_gospel(Config = #config { gospel = X, nodes = Nodes }) ->
+    Values = [reset | [N || {N, _} <- Nodes]],
+    [Config #config { gospel = V } || V <- Values, V =/= X ].
+
+add_node_to_cluster(Config = #config { nodes = ConfigNodes }, Namer) ->
+    %% We make this lazy so that we don't cause the node_namer to
+    %% churn through names
+    [fun () ->
+             Node = node_namer:get_name(Namer),
+             Config #config { nodes = [{Node, disc} | ConfigNodes] }
+     end |
+     [Config #config { nodes = [{Node, disc} | ConfigNodes] }
+      || Node <- node_namer:all_names(Namer),
+         not orddict:is_key(Node, ConfigNodes) ]
+    ].
+
+remove_node_from_cluster(Config = #config { nodes = ConfigNodes }) ->
+    [Config #config { nodes = orddict:erase(N, ConfigNodes) }
+     || {N, _} <- ConfigNodes].
--- test/src/node_namer.erl
+++ test/src/node_namer.erl
@@ -2,13 +2,16 @@
 
 -behaviour(gen_server).
 
--export([get_name/1]).
+-export([get_name/1, all_names/1]).
 
 -export([start_link/0, init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
 get_name(Pid) ->
     gen_server:call(Pid, get_name, infinity).
+
+all_names(Pid) ->
+    gen_server:call(Pid, all_names, infinity).
 
 start_link() ->
     {ok, _Pid} = gen_server:start_link(?MODULE, [], []).
@@ -20,6 +23,11 @@ init([]) ->
 handle_call(get_name, _From, {N, Host}) ->
     {reply, list_to_atom(lists:flatten(io_lib:format("node~p@~s", [N, Host]))),
      {N+1, Host}};
+handle_call(all_names, _From, {N, Host}) ->
+    {reply, 
+     [list_to_existing_atom(lists:flatten(io_lib:format("node~p@~s", [M, Host])))
+      || M <- lists:seq(0, N-1)],
+     {N, Host}};
 handle_call(Msg, From, State) ->
     {stop, {unhandled_call, Msg, From}, State}.
 
