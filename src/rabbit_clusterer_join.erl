60edf54a131d50420202e14e23ae3e373268d872
Matthew Sackman <matthew@rabbitmq.com>
2013-08-06 15:13:19 +0100

    Refactoring.

--- src/rabbit_clusterer_join.erl
+++ src/rabbit_clusterer_join.erl
@@ -39,21 +39,6 @@ event({comms, {Replies, BadNodes}}, State = #state { status  = awaiting_status,
         true ->
             {invalid_config, Config};
         false ->
-            %% Expected keys in StatusDict are:
-            %% - preboot:
-            %%    clusterer has started, but the boot step not yet hit
-            %% - {transitioner, rabbit_clusterer_join} (i.e. ?MODULE):
-            %%    it's joining some cluster - blocked in clusterer
-            %% - {transitioner, rabbit_clusterer_rejoin}:
-            %%    it's rejoining some cluster - blocked in clusterer
-            %% - booting:
-            %%    clusterer is happy and the rest of rabbit is
-            %%    currently booting
-            %% - ready:
-            %%    clusterer is happy and enough of rabbit has booted
-            %% - pending_shutdown:
-            %%    clusterer is waiting for the shutdown timeout and
-            %%    will then exit
             case rabbit_clusterer_utils:compare_configs(Youngest, Config) of
                 eq ->
                     %% We have the most up to date config. But we must
@@ -68,64 +53,8 @@ event({comms, {Replies, BadNodes}}, State = #state { status  = awaiting_status,
                             %% have to cope.
                             update_remote_nodes(OlderThanUs, Youngest, State);
                         [] ->
-                            State1 = State #state { config = Youngest },
-                            %% Everyone here has the same config, thus
-                            %% Statuses can be trusted as the statuses
-                            %% of all nodes trying to achieve *this*
-                            %% config and not some other config.
-                            ReadyNodes = lists:member(ready, Statuses),
-                            AllJoining = [{transitioner, ?MODULE}] =:= Statuses,
-                            %% ReadyNodes are nodes that are in this
-                            %% cluster (well, they could be in any
-                            %% cluster, but seeing as we've checked
-                            %% everyone has the same cluster config as
-                            %% us, we're sure it really is *this*
-                            %% cluster) and are fully up and running.
-                            %%
-                            %% If ReadyNodes exists, we should just
-                            %% reset and join into that, and ignore
-                            %% anything about gospel: it's possible
-                            %% that gospel is {node, node()} but that,
-                            %% in combination with ReadyNodes,
-                            %% suggests that the cluster previously
-                            %% existed with an older version of 'us':
-                            %% we must have been cleaned out and
-                            %% restarted (aka host-name reuse). Here
-                            %% we don't care about BadNodes.
-                            %%
-                            %% If ReadyNodes doesn't exist we can only
-                            %% safely proceed if there are no
-                            %% BadNodes, and everyone is joining
-                            %% (rather than rejoining) i.e. transition
-                            %% module for all is ?MODULE. In all other
-                            %% cases, we must wait:
-                            %%
-                            %% - If BadNodes =/= [] then there may be
-                            %%   a node that was cleanly shutdown last
-                            %%   with what we think is the current
-                            %%   config and so if it was started up,
-                            %%   it would rejoin (itself, sort of) and
-                            %%   then become ready: we could then sync
-                            %%   to it.
-                            %%
-                            %% - If the transition module is not all
-                            %%   ?MODULE then some other nodes must be
-                            %%   rejoining. We should wait for them to
-                            %%   succeed (or at least change state)
-                            %%   because if they do succeed we should
-                            %%   sync off them.
-                            case ReadyNodes of
-                                true ->
-                                    ok = cluster_with_nodes(Youngest),
-                                    {success, Youngest};
-                                false when AllJoining andalso BadNodes =:= [] ->
-                                    case maybe_form_new_cluster(Youngest) of
-                                        true  -> {success, Youngest};
-                                        false -> delayed_request_status(State1)
-                                    end;
-                                false ->
-                                    delayed_request_status(State1)
-                            end
+                            maybe_join(BadNodes =:= [], Statuses,
+                                       State #state { config = Youngest })
                     end;
                 _ ->
                     {config_changed, Youngest}
@@ -189,6 +118,67 @@ update_remote_nodes(Nodes, Config, State = #state { comms = Comms }) ->
     Msg = rabbit_clusterer_coordinator:template_new_config(Config),
     ok = rabbit_clusterer_comms:multi_cast(Nodes, Msg, Comms),
     delayed_request_status(State).
+
+maybe_join(AllGoodNodes, Statuses, State = #state { config = Config }) ->
+    %% Everyone here has the same config, thus Statuses can be trusted
+    %% as the statuses of all nodes trying to achieve *this* config
+    %% and not some other config.
+    %%
+    %% Expected entries in Statuses are:
+    %% - preboot:
+    %%    clusterer has started, but the boot step not yet hit
+    %% - {transitioner, rabbit_clusterer_join} (i.e. ?MODULE):
+    %%    it's joining some cluster - blocked in clusterer
+    %% - {transitioner, rabbit_clusterer_rejoin}:
+    %%    it's rejoining some cluster - blocked in clusterer
+    %% - booting:
+    %%    clusterer is happy and the rest of rabbit is currently
+    %%    booting
+    %% - ready:
+    %%    clusterer is happy and enough of rabbit has booted
+    %% - pending_shutdown:
+    %%    clusterer is waiting for the shutdown timeout and will then
+    %%    exit
+    ReadyNodes = lists:member(ready, Statuses),
+    AllJoining = [{transitioner, ?MODULE}] =:= Statuses,
+    %% ReadyNodes are nodes that are in this cluster (well, they could
+    %% be in any cluster, but seeing as we've checked everyone has the
+    %% same cluster config as us, we're sure it really is *this*
+    %% cluster) and are fully up and running.
+    %%
+    %% If ReadyNodes exists, we should just reset and join into that,
+    %% and ignore anything about gospel: it's possible that gospel is
+    %% {node, node()} but that, in combination with ReadyNodes,
+    %% suggests that the cluster previously existed with an older
+    %% version of 'us': we must have been cleaned out and restarted
+    %% (aka host-name reuse). Here we don't care about BadNodes.
+    %%
+    %% If ReadyNodes doesn't exist we can only safely proceed if there
+    %% are no BadNodes, and everyone is joining (rather than
+    %% rejoining) i.e. transition module for all is ?MODULE. In all
+    %% other cases, we must wait:
+    %%
+    %% - If BadNodes =/= [] then there may be a node that was cleanly
+    %% shutdown last with what we think is the current config and so
+    %% if it was started up, it would rejoin (itself, sort of) and
+    %% then become ready: we could then sync to it.
+    %%
+    %% - If the transition module is not all ?MODULE then some other
+    %% nodes must be rejoining. We should wait for them to succeed (or
+    %% at least change state) because if they do succeed we should
+    %% sync off them.
+    case ReadyNodes of
+        true ->
+            ok = cluster_with_nodes(Config),
+            {success, Config};
+        false when AllJoining andalso AllGoodNodes ->
+            case maybe_form_new_cluster(Config) of
+                true  -> {success, Config};
+                false -> delayed_request_status(State)
+            end;
+        false ->
+            delayed_request_status(State)
+    end.
 
 cluster_with_nodes(#config { nodes = Nodes }) ->
     ok = rabbit_clusterer_utils:wipe_mnesia(),
