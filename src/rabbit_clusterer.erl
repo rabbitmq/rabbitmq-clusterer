a627c9a869c5df5fce8c743763f1b12857ea7823
Matthias Radestock <matthias@rabbitmq.com>
2013-08-06 11:13:00 +0100

    tidy exports, cosmetic

--- src/rabbit_clusterer.erl
+++ src/rabbit_clusterer.erl
@@ -2,9 +2,11 @@
 
 -behaviour(application).
 
--export([start/2, stop/1, boot/0, apply_config/0, apply_config/1]).
+-export([start/2, stop/1, boot/0]).
 
--export([begin_clustering/0, rabbit_booted/0]).
+-export([apply_config/0, apply_config/1]). %% for 'rabbitmqctl eval ...'
+
+-export([rabbit_booted/0]).
 
 -rabbit_boot_step(
    {rabbit_clusterer,
@@ -14,13 +16,11 @@
 
 -define(APP, rabbitmq_clusterer).
 
-start(normal, []) ->
-    rabbit_clusterer_sup:start_link().
+start(normal, []) -> rabbit_clusterer_sup:start_link().
 
-stop(_State) ->
-    ok.
+stop(_State) -> ok.
 
-begin_clustering() ->
+boot() ->
     ok = rabbit_clusterer_utils:stop_mnesia(),
     %% We need to ensure the app is already started:
     ok = application:ensure_started(?APP),
@@ -31,11 +31,6 @@ begin_clustering() ->
 rabbit_booted() ->
     ok = rabbit_clusterer_coordinator:rabbit_booted().
 
-boot() ->
-    ok = begin_clustering().
+apply_config() -> apply_config(undefined).
 
-apply_config() ->
-    apply_config(undefined).
-
-apply_config(Config) ->
-    rabbit_clusterer_coordinator:apply_config(Config).
+apply_config(Config) -> rabbit_clusterer_coordinator:apply_config(Config).
