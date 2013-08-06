b895c83d9e8b5d23619d962177a3217b603490f2
Matthias Radestock <matthias@rabbitmq.com>
2013-08-06 11:37:50 +0100

    cosmetic(ish) refactoring

--- src/rabbit_clusterer.erl
+++ src/rabbit_clusterer.erl
@@ -2,26 +2,23 @@
 
 -behaviour(application).
 
--export([start/2, stop/1, boot/0]).
+-export([boot/0]).
+
+-export([rabbit_booted/0]).
 
 -export([apply_config/0, apply_config/1]). %% for 'rabbitmqctl eval ...'
 
--export([rabbit_booted/0]).
+-export([start/2, stop/1]).
 
 -rabbit_boot_step({rabbit_clusterer,
                    [{description, "Declarative Clustering"},
                     {mfa, {?MODULE, rabbit_booted, []}},
                     {requires, networking}]}).
 
--define(APP, rabbitmq_clusterer).
-
-start(normal, []) -> rabbit_clusterer_sup:start_link().
-
-stop(_State) -> ok.
+%%----------------------------------------------------------------------
 
 boot() ->
-    %% We need to ensure the app is already started:
-    ok = application:ensure_started(?APP),
+    ok = application:start(rabbitmq_clusterer),
     ok = rabbit_clusterer_coordinator:begin_coordination(),
     ok.
 
@@ -34,3 +31,10 @@ rabbit_booted() ->
 apply_config() -> apply_config(undefined).
 
 apply_config(Config) -> rabbit_clusterer_coordinator:apply_config(Config).
+
+%%----------------------------------------------------------------------
+
+start(normal, []) -> rabbit_clusterer_sup:start_link().
+
+stop(_State) -> ok.
+
