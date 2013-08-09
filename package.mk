a611e1e1682e822f49e7390abc936a68f5958408
Matthew Sackman <matthew@rabbitmq.com>
2013-08-09 13:03:41 +0100

    Add dialyze target

--- package.mk
+++ package.mk
@@ -1,2 +1,14 @@
 DEPS:=rabbitmq-server
 STANDALONE_TEST_COMMANDS:=test:test()
+
+define package_rules
+
+.PHONY: dialyze
+dialyze: $(EBIN_BEAMS) $(PACKAGE_DIR)/../rabbitmq-server/rabbit.plt
+	dialyzer --plt $(PACKAGE_DIR)/../rabbitmq-server/rabbit.plt --no_native --fullpath $(EBIN_BEAMS)
+
+.PHONY: $(PACKAGE_DIR)/../rabbitmq-server/rabbit.plt
+$(PACKAGE_DIR)/../rabbitmq-server/rabbit.plt:
+	$(MAKE) -C $(PACKAGE_DIR)/../rabbitmq-server rabbit.plt
+
+endef
