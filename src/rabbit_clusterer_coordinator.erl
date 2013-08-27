82568d233d379d498e55fcbdf9d36ab658708cf4
Matthew Sackman <matthew@rabbitmq.com>
2013-08-27 15:49:50 +0100

    Reinstate but correct changeset originally from 1ee8387b9176.

--- src/rabbit_clusterer_coordinator.erl
+++ src/rabbit_clusterer_coordinator.erl
@@ -384,10 +384,7 @@ set_status(NewStatus, State = #state { status = Status })
 set_status(pending_shutdown, State = #state { status = ready }) ->
     %% Even though we think we're ready, there might still be some
     %% rabbit boot actions going on...
-    error_logger:info_msg("Clusterer stopping Rabbit pending shutdown.~n"),
-    ok = rabbit:await_startup(),
-    ok = rabbit_clusterer_utils:stop_rabbit(),
-    ok = rabbit_clusterer_utils:stop_mnesia(),
+    ok = stop_rabbit(),
     State #state { status = pending_shutdown };
 set_status(pending_shutdown, State = #state { status = Status })
   when Status =/= booting ->
@@ -426,23 +423,25 @@ begin_transition(NewConfig, State = #state { config = Config }) ->
                    NewConfig, State)
     end.
 
-begin_transition(false, NewConfig, State = #state { status = ready }) ->
-    %% Must use the un-merged config here otherwise we risk getting a
-    %% different answer from is_compatible when we come back here
-    %% after reboot.
-    begin_transition(NewConfig, set_status(pending_shutdown, State));
-begin_transition(true, NewConfig, State = #state { status  = ready,
-                                                   node_id = NodeID,
-                                                   config  = Config }) ->
+begin_transition(true,     NewConfig, State = #state { status  = ready,
+                                                       node_id = NodeID,
+                                                       config  = Config }) ->
     NewConfig1 = rabbit_clusterer_config:transfer_node_ids(Config, NewConfig),
     ok = rabbit_clusterer_config:store_internal(NodeID, NewConfig1),
     error_logger:info_msg(
       "Clusterer seemlessly transitioned to new configuration:~n~p~n",
       [rabbit_clusterer_config:to_proplist(NodeID, NewConfig1)]),
     update_monitoring(State #state { config = NewConfig1 });
-begin_transition(Compatible, NewConfig, State = #state { node_id = NodeID,
-                                                         config  = Config,
-                                                         nodes   = Nodes }) ->
+begin_transition(false,    NewConfig, State = #state { status = ready }) ->
+    join_or_rejoin(join,   NewConfig, set_status(pending_shutdown, State));
+begin_transition(true,     NewConfig, State) ->
+    join_or_rejoin(rejoin, NewConfig, State);
+begin_transition(false,    NewConfig, State) ->
+    join_or_rejoin(join,   NewConfig, State).
+
+join_or_rejoin(TKind, NewConfig, State = #state { node_id = NodeID,
+                                                  config  = Config,
+                                                  nodes   = Nodes }) ->
     NewConfig1 = rabbit_clusterer_config:transfer_node_ids(Config, NewConfig),
     ok = send_new_config(NewConfig1, Nodes),
     %% Wipe out alive_mrefs and dead so that if we get DOWN's we don't
@@ -450,10 +449,6 @@ begin_transition(Compatible, NewConfig, State = #state { node_id = NodeID,
     {Comms, State1} = fresh_comms(State #state { alive_mrefs = [],
                                                  dead        = [],
                                                  nodes       = [] }),
-    TKind = case Compatible of
-                true  -> rejoin;
-                false -> join
-            end,
     process_transitioner_response(
       rabbit_clusterer_transitioner:init(TKind, NodeID, NewConfig1, Comms),
       set_status({transitioner, TKind}, State1)).
@@ -531,6 +526,17 @@ reschedule_shutdown(State = #state { status             = pending_shutdown,
     schedule_shutdown(State);
 reschedule_shutdown(State) ->
     State.
+
+%%----------------------------------------------------------------------------
+%% Helpers
+%%----------------------------------------------------------------------------
+
+stop_rabbit() ->
+    error_logger:info_msg("Clusterer stopping Rabbit pending shutdown.~n"),
+    ok = rabbit:await_startup(),
+    ok = rabbit_clusterer_utils:stop_rabbit(),
+    ok = rabbit_clusterer_utils:stop_mnesia(),
+    ok.
 
 update_monitoring(State = #state { config = ConfigNew,
                                    nodes  = NodesOld }) ->
