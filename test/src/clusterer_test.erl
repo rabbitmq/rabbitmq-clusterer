2c318ebbe6e732fbf104c6c25e6eafb4905563f1
Matthew Sackman <matthew@rabbitmq.com>
2013-08-27 15:50:20 +0100

    Wire into the test harness properly so that "make test" does work.

--- test/src/clusterer_test.erl
+++ test/src/clusterer_test.erl
@@ -1,45 +1,55 @@
 -module(clusterer_test).
 
--export([test/0, test/1]).
+-export([test/1]).
 
 -include("clusterer_test.hrl").
 
-test() ->
+%% NB Limit is exclusive, not inclusive.
+test(Limit) when Limit > 0 ->
     case node() of
         'nonode@nohost' -> {error, must_be_distributed_node};
-        _               -> test(0)
+        _               -> test_sequence(0, Limit)
     end.
 
-test(Seed) ->
-    [$@|Host] = lists:dropwhile(fun (C) -> C =/= $@ end, atom_to_list(node())),
-    test(Host, Seed).
+test_sequence(Limit, Limit) ->
+    io:format("No programs between 0 and ~p failed.~n", [Limit]),
+    ok;
+test_sequence(N, Limit) ->
+    case test_program(N) of
+        ok  -> test_sequence(N+1, Limit);
+        Err -> io:format("Error encountered with program ~p:~n~p~n",
+                         [N, Err]),
+               Err
+    end.
 
-test(Host, Seed) ->
-    State = #test { seed          = Seed,
-                    namer         = {0, Host},
-                    nodes         = orddict:new(),
-                    config        = #config { nodes            = [],
-                                              gospel           = reset,
-                                              shutdown_timeout = infinity,
-                                              version          = 0 },
-                    valid_config  = undefined,
-                    active_config = undefined
-                  },
+test_program(Seed) ->
+    State = new_state(Seed),
     Program = clusterer_program:generate_program(State),
-    case starts_nodes(Program) of
-        true ->
-            io:format("Starting interesting program number ~p:~n~p~n~n",
-                      [Seed, Program]),
-            case clusterer_interpreter:run_program(Program, State) of
-                ok -> {Program, success};
-                E  -> io:format("Error encountered with program~n~p~n~p~n~n",
-                                [Program, E]),
-                      {Program, E}
-            end;
-        false ->
-            uninteresting
+    case filter_program(Program) of
+        skip -> ok;
+        run  -> io:format("~p...", [Seed]),
+                clusterer_interpreter:run_program(Program, State)
     end.
 
+new_state(Seed) ->
+    [$@|Host] = lists:dropwhile(fun (C) -> C =/= $@ end, atom_to_list(node())),
+    #test { seed          = Seed,
+            namer         = {0, Host},
+            nodes         = orddict:new(),
+            config        = #config { nodes            = [],
+                                      gospel           = reset,
+                                      shutdown_timeout = infinity,
+                                      version          = 0 },
+            valid_config  = undefined,
+            active_config = undefined
+          }.
+
+filter_program(Program) ->
+    %% Eventually there'll be a more sophisticated set of filters here.
+    case starts_nodes(Program) of
+        true  -> run;
+        false -> skip
+    end.
 
 starts_nodes(Program) ->
     starts_nodes(Program, orddict:new()).
