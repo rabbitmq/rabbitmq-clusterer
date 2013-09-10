95810263994e7b20681b9af52bd74fb4f617299b
Matthew Sackman <matthew@rabbitmq.com>
2013-09-10 12:53:58 +0100

    Wrote a brief essay of sorts.

--- test/src/clusterer_test.erl
+++ test/src/clusterer_test.erl
@@ -5,6 +5,87 @@
 -include("clusterer_test.hrl").
 
 %%----------------------------------------------------------------------------
+%%
+%% Testing the Clusterer
+%%
+%% Testing the Clusterer is challenging given the level of
+%% concurrency, eventually-consistent design, and the vast number of
+%% scenarios and modifications possible. The approach we take here is
+%% to deterministically generate programs which are then filtered and
+%% interpreted. These programs describe how to construct up to one
+%% cluster and operations upon the cluster.
+%%
+%% A program is a sequence of steps from a given starting state. Each
+%% step contains up to one instruction for each node, up to one
+%% instruction for modifying the cluster config, and up to one
+%% instruction for creating or deleting a node. All instructions are
+%% independent thus all the instructions within a step can be run in
+%% parallel. For example, a step can not contain both a "switch node X
+%% on" and a "delete node X" instruction. During program generation we
+%% capture the expected state of all the nodes at the end of each
+%% step. When we come to interpret the program, we use this expected
+%% state to compare to what we observe of the Real World. If we reach
+%% the end of the program and no divergence is observed then the
+%% program passed successfully.
+%%
+%% Program generation is driven by a seed. When the seed is reduced to
+%% 0, there is no more entropy available, and so program generation
+%% halts. At each step, the set of available instructions to choose
+%% from is highly dependent on the predicted state of the nodes at
+%% this point. Having constructed a list of viable instructions at a
+%% given point in the program, the seed is used to select the
+%% instruction. The list will always include the 'noop' instruction
+%% (but will never be just the 'noop' instruction), and the seed
+%% modulo the length of the list of valid instructions is used to
+%% select which instruction is chosen. The seed is the divided by the
+%% length of that list. Thus the entropy of the seed is slowly
+%% reduced: it is consumed by selecting instructions from lists. This
+%% strategy means every seed will result in a unique program (though
+%% isomorphisms of various degrees are possible), and that a given
+%% seed will always generate the same program. In other words, the
+%% seed is equivalent to the perfect compression of the program (proof
+%% left as an exercise to the reader...).
+%%
+%% Program generation is heavily constrained to avoid generating
+%% programs where there are multiple possible valid outcomes, and to
+%% ensure we never have more than a single cluster running. The latter
+%% is essential to keep modelling of the nodes feasible.
+%%
+%% Having generated a program we then filter it to test whether or not
+%% in contains any interesting aspects. Currently we just look for
+%% programs which contain more than a single node running at any given
+%% point in time, but more elaborate filters are possible. Having
+%% selected a program to run, it is then passed to the interpreter.
+%%
+%% Some aspects of the interpreter are remarkably similar to the
+%% program generation itself: in the program generator we have to
+%% model changes to nodes and cluster configs and certainly much of
+%% the mechanism for making changes to the cluster configs is very
+%% similar in the interpreter. For nodes, we have one process per node
+%% to allow the possibility of changes to the nodes themselves for
+%% each step actually occurring in parallel. In this regard, given the
+%% program doesn't really specify scheduling of instructions beyond
+%% "concurrently", multiple different runs of the same program may
+%% result in different instructions being evaluated at different
+%% times. However, the point of the Clusterer (and the constraints of
+%% the program generation) is that it should be eventually consistent:
+%% the outcome of each step should be the independent of individual
+%% scheduling of instructions within a step.
+%%
+%% To detect divergence we have to be aware that a cluster of nodes
+%% may take a short period of time to stabilise at a new cluster
+%% config, to turn off, etc. The strategy we adopt is not fool-proof,
+%% but it'll do. We wait for all nodes to be stable in some way
+%% (i.e. off, reset, on or pending_shutdown, but not booting). We then
+%% wait a short amount of time and ask them all again for their
+%% state. If they're all still stable and they're all still stable in
+%% the same way, then we declare the cluster is stable and that we can
+%% actually check this state for divergence. At this point we compare
+%% their stable states and the cluster configs they're running with
+%% the state our program generation predicted for each step and fail
+%% if any divergence is detected.
+%%
+%%----------------------------------------------------------------------------
 
 %% NB Limit is exclusive, not inclusive.
 test(Limit) when Limit > 0 ->
